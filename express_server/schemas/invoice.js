400d169b04396a2ff97a6c366118341d038f32bd
lazyKT <kyaw.thitlwin.me@gmail.com>
2021-11-20 22:57:14 +0800

    Invoice Schemas and GET/POST Routes for Pharmacy

---
+++ express_server/schemas/invoice.js
@@ -0,0 +1,109 @@
+const mongoose = require("mongoose");
+const Joi = require("joi")
+              .extend(require("@joi/date"));
+
+
+const cartItemSchema = new mongoose.Schema({
+  productNumber: {
+    type: String,
+    required: true
+  },
+  productName: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  qty: {
+    type: Number,
+    required: true
+  },
+  totalPrice: {
+    type: Number,
+    required: true
+  },
+  remark: {
+    type: String
+  }
+});
+
+
+function validateCartItem (item) {
+  const schema = Joi.object({
+    productNumber: Joi.string().required(),
+    price: Joi.number().required(),
+    qty: Joi.number().required(),
+    totalPrice: Joi.number().required()
+  });
+
+  const validateResult = schema.validate(item);
+  return validateResult;
+}
+
+
+const pharmacyInvoiceSchema = new mongoose.Schema({
+  invoiceNumber: {
+    type: String,
+    min: 14,
+    required: true,
+    index: { unique: true }
+  },
+  employeeID: {
+    type: String,
+    required: true
+  },
+  customerID: {
+    type: String,
+    required: true
+  },
+  cartItems: [ cartItemSchema ],
+  payableAmount: {
+    type: Number,
+    required: true
+  },
+  givenAmount: {
+    type: Number,
+    required: true
+  },
+  changeAmount: {
+    type: Number,
+    required: true
+  },
+  created: {
+    type: Date,
+    default: new Date()
+  }
+});
+
+
+function validateInvoice (invoice) {
+  const schema = Joi.object({
+    invoiceNumber: Joi.string().min(14).required(),
+    employeeID: Joi.string().required(),
+    customerID: Joi.string().required(),
+    payableAmount: Joi.number().required(),
+    cartItems: Joi.array().items({
+        productNumber: Joi.string().required(),
+        productName: Joi.string().required(),
+        price: Joi.number().required(),
+        qty: Joi.number().required(),
+        totalPrice: Joi.number().required()
+    }),
+    givenAmount: Joi.number().required(),
+    changeAmount: Joi.number().required()
+  });
+
+  const validateResult = schema.validate(invoice);
+  return validateResult;
+}
+
+
+
+const PharmacyInvoice = new mongoose.model ("Invoice", pharmacyInvoiceSchema);
+
+
+exports.PharmacyInvoice = PharmacyInvoice;
+exports.validateInvoice = validateInvoice;
+exports.validateCartItem = validateCartItem;
